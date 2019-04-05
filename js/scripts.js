b61ce15a3bce952a124aa0883b7a72f6da7fe7b1
colton lacey <coltnutmiles14@gmail.com>
2019-04-05 14:24:34 -0700

    fix var names in for loops

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,22 @@ var beepBoopArray = [];
 
 
 //this will change numbers 1, 2, and 3 to "Beep!", "Boop!"", and "I'm sorry, Dave. I'm afraid I can't do that".
-var numbersBreakDown = function(num) {
-  for (i = num; i < num.lenght; i++) {
-    if (i === 1){
+function numberBreakDown(num) {
+  for (i = 0; i < num.lenght; i++) {
+    if (num[i] === 1){
       beepBoopArray.push("Beep!");
-    } else if (i === 2) {
+    } else if (num[i] === 2) {
       beepBoopArray.push("Boop!");
-    } else if (i === 3) {
+    } else if (num[i] === 3) {
       beepBoopArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      beepBoopArray.push(i);
+      beepBoopArray.push(num[i]);
     }
 
   }
 }
 
-
+console.log(beepBoopArray);
 
 
 
