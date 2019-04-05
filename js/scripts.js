e4847d69517202f962090b07a5ae38b3088e893a
colton lacey <coltnutmiles14@gmail.com>
2019-04-05 13:34:38 -0700

    add if statmetn to handle numbers 1 2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 // business logic //
 
-var beepBoopArray
+var beepBoopArray = [];
 
 
-
+//this will change numbers 1, 2, and 3 to "Beep!", "Boop!"", and "I'm sorry, Dave. I'm afraid I can't do that".
 var numbersBreakDown = function(num) {
-  for (i = num; i < num.lenght; i++){
+  for (i = num; i < num.lenght; i++) {
     if (i === 1){
-
+      beepBoopArray.push("Beep!");
+    } else if (i === 2) {
+      beepBoopArray.push("Boop!");
+    } else if (i === 3) {
+      beepBoopArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      beepBoopArray.push(i);
     }
 
   }
